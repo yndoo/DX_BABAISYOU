e972171a2b1d363c7d523364ebf104a9f52ed597
yndoo 배연두 <yndoo2k@gmail.com>
2024-05-02 14:15:34 +0900

    배연두 : 엔진 업데이트

--- DX_BABAISYOU/EngineCore/Image.cpp
+++ DX_BABAISYOU/EngineCore/Image.cpp
@@ -56,7 +56,7 @@ void UIAnimation::Update(float _DeltaTime)
 UImage::UImage() 
 {
 	SetMesh("Rect");
-	SetMaterial("2DImage");
+	SetMaterial("Widget2DImage");
 }
 
 UImage::~UImage() 
@@ -148,6 +148,15 @@ void UImage::SetSpriteInfo(const FSpriteInfo& _Info)
 		float4 Scale = Transform.WorldScale;
 		Scale.X = -abs(Scale.X) * 0.5f;
 		Scale.Y = abs(Scale.Y) * 0.5f;
+		Scale.Z = 0.0f;
+		CuttingDataValue.PivotMat.Position(Scale);
+		break;
+	}
+	case EPivot::RIGHTTOP:
+	{
+		float4 Scale = Transform.WorldScale;
+		Scale.X = -abs(Scale.X) * 0.5f;
+		Scale.Y = -abs(Scale.Y) * 0.5f;
 		Scale.Z = 0.0f;
 		CuttingDataValue.PivotMat.Position(Scale);
 		break;
@@ -356,4 +365,14 @@ void UImage::CreateAnimation(std::string_view _AnimationName, std::string_view _
 bool UImage::IsCurAnimationEnd()
 {
 	return CurAnimation->IsEnd;
+}
+
+void UImage::SetDir(EEngineDir _Dir)
+{
+	Dir = _Dir;
+
+	if (nullptr != CurInfo.Texture)
+	{
+		SetSpriteInfo(CurInfo);
+	}
 }
